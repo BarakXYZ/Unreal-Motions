94dda37844f74965620bc4532582a4f5aaea0d61
BarakXYZ <iBarakXYZ@gmail.com>
2024-12-12 00:58:45 +0200

    Detect Vim Mode: Insert vs. Normal vs. Visual Mode (WIP)

--- Source/ErgonomicsXYZ/Public/ErgonomicsXYZ.h
+++ Source/ErgonomicsXYZ/Public/ErgonomicsXYZ.h
@@ -8,47 +8,56 @@
 #include "Widgets/Docking/SDockTab.h"
 #include "Widgets/SWidget.h"
 
+UENUM(BlueprintType)
+enum class EVimMode : uint8
+{
+    Normal UMETA(DisplayName = "Normal"),
+    Insert UMETA(DisplayName = "Insert"),
+    Visual UMETA(DisplayName = "Visual"),
+};
+
 class FErgonomicsXYZModule : public IModuleInterface
 {
-public:
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
+  public:
+    /** IModuleInterface implementation */
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
 
-public: // Methods
-	void InitTabNavInputChords(
-		bool bCtrl = true, bool bAlt = false,
-		bool bShift = true, bool bCmd = false);
+  public: // Methods
+    void InitTabNavInputChords(bool bCtrl = true, bool bAlt = false, bool bShift = true, bool bCmd = false);
 
-	void RemoveDefaultCommand(FInputBindingManager& IBManager, FInputChord Cmd);
-	void AddCommandsToList(TSharedPtr<FBindingContext> MainFrameContext);
-	void MapTabCommands(TSharedRef<FUICommandList>& CommandList);
+    void RemoveDefaultCommand(FInputBindingManager &IBManager, FInputChord Cmd);
+    void AddCommandsToList(TSharedPtr<FBindingContext> MainFrameContext);
+    void MapTabCommands(TSharedRef<FUICommandList> &CommandList);
 
-	void RemoveCommand();
-	void OnMoveToTab(int32 TabIndex);
-	bool TraverseWidgetTree(const TSharedPtr<SWidget>& TraverseWidget, TSharedPtr<SWidget>& DockingTabWell, int32 Depth = 0);
-	void FocusTab(const TSharedPtr<SWidget>& DockingTabWell, int TabIndex);
-	void OnPreInputKeyDown(const FKeyEvent& KeyEvent);
+    void RemoveCommand();
+    void OnMoveToTab(int32 TabIndex);
+    bool TraverseWidgetTree(const TSharedPtr<SWidget> &TraverseWidget, TSharedPtr<SWidget> &DockingTabWell,
+                            int32 Depth = 0);
+    void FocusTab(const TSharedPtr<SWidget> &DockingTabWell, int TabIndex);
+    void OnPreInputKeyDown(const FKeyEvent &KeyEvent);
+    void DetectVimMode(const FKeyEvent &KeyEvent);
 
-public: // Members
-	// FSlateApplication*			  SlateApp = nullptr;
+  public: // Members
+    // FSlateApplication*			  SlateApp = nullptr;
 
-	/*
-	 * Mentioned in editor preferences as "System-wide" category
-	 * Another option is MainFrame
-	 * The general workflow to find the specific naming though is just
-	 * to live grep and look for something similar.
-	 * If not using the very specific names, the engine won't be able to open at all.
-	 */
-	const FName		MainFrameContextName = TEXT("MainFrame");
-	FDelegateHandle PreInputKeyDownDelegateHandle;
+    /*
+     * Mentioned in editor preferences as "System-wide" category
+     * Another option is MainFrame
+     * The general workflow to find the specific naming though is just
+     * to live grep and look for something similar.
+     * If not using the very specific names, the engine won't be able to open at all.
+     */
+    const FName MainFrameContextName = TEXT("MainFrame");
+    FDelegateHandle PreInputKeyDownDelegateHandle;
 
-	TSharedPtr<class FUICommandInfo>   CommandInfoTab1 = nullptr;
-	TArray<TSharedPtr<FUICommandInfo>> CommandInfoTabs = {
-		nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr
-	};
+    TSharedPtr<class FUICommandInfo> CommandInfoTab1 = nullptr;
+    TArray<TSharedPtr<FUICommandInfo>> CommandInfoTabs = {nullptr, nullptr, nullptr, nullptr, nullptr,
+                                                          nullptr, nullptr, nullptr, nullptr, nullptr};
 
-	TSharedPtr<class FUICommandInfo>  _openLevelSequenceCommandInfo = nullptr;
-	TMap<FName, TSharedRef<SDockTab>> OpenTabs;
-	TArray<FInputChord>				  TabChords;
+    TSharedPtr<class FUICommandInfo> _openLevelSequenceCommandInfo = nullptr;
+    TMap<FName, TSharedRef<SDockTab>> OpenTabs;
+    TArray<FInputChord> TabChords;
+    bool bIsNormalMode = false;
+    EVimMode VimMode = EVimMode::Insert;
 };
